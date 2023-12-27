09a138c384830406426d0e51ba117836cba9de1c
dantas-barreto <jpsdb10@hotmail.com>
2023-12-27 13:33:50 -0300

    update

--- src/polinomios.c
+++ src/polinomios.c
@@ -1,6 +1,3 @@
-    // Um polinomio de grau g pode ser representado por um vetor v com g + 1 elementos, representando os coeficientes do polinomio: v[i] = a
-    // Assim, o polinomio 2x^3 + 8x + 5 pode ser representado por um vetor com 4 elementos: v[0] = 5, v[1] = 8, v[2] = 0 e v[3] = 2.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
